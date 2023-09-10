9486bdbf18208eb3905a37e11ce21b757599f72e
wesley <wesleybruno910@gmail.com>
2023-09-10 16:11:54 -0300

    feat: add findByNome() method to find a Course by name and return its id

--- src/main/java/com/br/alura/forum/repository/CursoRepository.java
+++ src/main/java/com/br/alura/forum/repository/CursoRepository.java
@@ -2,8 +2,13 @@ package com.br.alura.forum.repository;
 
 import com.br.alura.forum.modelo.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Long> {
+    @Query("SELECT c.id FROM Curso c WHERE c.nome LIKE %:nome% ORDER BY c.nome")
+    List<Long> findByNome(String nome);
 }
