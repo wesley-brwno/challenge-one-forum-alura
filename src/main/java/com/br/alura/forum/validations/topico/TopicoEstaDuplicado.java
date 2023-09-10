22fe05b6dfef2f371df76ad068196a798f9cae7f
wesley <wesleybruno910@gmail.com>
2023-09-10 19:07:15 -0300

    refector: adds isAutorOrCursoEdited to TopicoEstaDuplicado
    This method return true or false if the id of a curse or author is edited. This was done due to the validar() implementation in this class throw an exception, thus avoiding the update of Topico entity.

--- src/main/java/com/br/alura/forum/validations/topico/TopicoEstaDuplicado.java
+++ src/main/java/com/br/alura/forum/validations/topico/TopicoEstaDuplicado.java
@@ -1,10 +1,13 @@
 package com.br.alura.forum.validations.topico;
 
 import com.br.alura.forum.DTO.topico.CadastrarTopicoDados;
+import com.br.alura.forum.modelo.Topico;
 import com.br.alura.forum.repository.TopicoRepository;
 import com.br.alura.forum.validations.ValidationsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class TopicoEstaDuplicado implements ValidarDadosTopico{
@@ -12,10 +15,15 @@ public class TopicoEstaDuplicado implements ValidarDadosTopico{
     private TopicoRepository repository;
     @Override
     public void validar(CadastrarTopicoDados dados) {
-        if (repository.countByTitulo(dados.titulo()) > 0) {
-            if (repository.countByMensagem(dados.mensagem()) > 0) {
+        if (repository.countByTitulo(dados.titulo()) > 0 && repository.countByMensagem(dados.mensagem()) > 0) {
+            if (!isAutorOrCursoEdited(dados)) {
                 throw new ValidationsException("Tópico já existe");
             }
         }
+    }
+
+    public boolean isAutorOrCursoEdited(CadastrarTopicoDados dados) {
+        Topico topico = repository.findByTituloAndMensagem(dados.titulo(), dados.mensagem());
+        return !Objects.equals(topico.getAutor().getId(), dados.autorId()) || !Objects.equals(topico.getCurso().getId(), dados.cursoId());
     }
 }
