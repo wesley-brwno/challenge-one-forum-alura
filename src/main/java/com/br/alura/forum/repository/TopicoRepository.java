859e5ed4bc4bc8cb57c9f1840c6a9ea1869ce883
wesley <wesleybruno910@gmail.com>
2023-09-10 16:13:53 -0300

    feat: add findAllByIdNotNull(), findAllByCuro_IdOrderByDataCriacao(), findByAno() methods to allow personalised queries

--- src/main/java/com/br/alura/forum/repository/TopicoRepository.java
+++ src/main/java/com/br/alura/forum/repository/TopicoRepository.java
@@ -1,11 +1,18 @@
 package com.br.alura.forum.repository;
 
 import com.br.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    Page<Topico> findAllByIdNotNull(Pageable pageable);
+    Page<Topico> findAllByCurso_IdOrderByDataCriacao(Pageable pageable, Long id);
+    @Query("SELECT t FROM Topico t WHERE YEAR(t.dataCriacao) = :ano ORDER BY t.titulo ASC")
+    Page<Topico> findByAno(Pageable pageable, Long ano);
     long countByTitulo(String titulo);
     long countByMensagem(String mensagem);
 }
