feec8b50dd7cfd5193d57e2753115825f8675633
wesley <wesleybruno910@gmail.com>
2023-09-12 08:37:33 -0300

    feat: add findAllByIdNotNullOrderByNome method to UsuarioRespository inteface
    This method allows to query all Usuarios where their id is not null, ordered by nome. The method returns a Page of Usuario, enabling pagination features

--- src/main/java/com/br/alura/forum/repository/UsuarioRespository.java
+++ src/main/java/com/br/alura/forum/repository/UsuarioRespository.java
@@ -1,9 +1,12 @@
 package com.br.alura.forum.repository;
 
 import com.br.alura.forum.modelo.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRespository extends JpaRepository<Usuario, Long> {
+    Page<Usuario> findAllByIdNotNullOrderByNome(Pageable pageable);
 }
