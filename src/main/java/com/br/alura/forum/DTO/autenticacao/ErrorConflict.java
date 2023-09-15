0077d779c4094a8084aeed2a38f9769277e2a590
wesley <wesleybruno910@gmail.com>
2023-09-15 15:35:46 -0300

    feat: add ErrorConflict,TokenDTO record classes
    The tokenDTO is used to sent token in json format
    
    The ErrorConflict is used to sent an error mesnage in json format for the 409 Conflict http response

---
+++ src/main/java/com/br/alura/forum/DTO/autenticacao/ErrorConflict.java
@@ -0,0 +1,4 @@
+package com.br.alura.forum.DTO.autenticacao;
+
+public record ErrorConflict(String error) {
+}
---
+++ src/main/java/com/br/alura/forum/DTO/autenticacao/TokenDTO.java
@@ -0,0 +1,4 @@
+package com.br.alura.forum.DTO.autenticacao;
+
+public record TokenDTO(String token) {
+}
