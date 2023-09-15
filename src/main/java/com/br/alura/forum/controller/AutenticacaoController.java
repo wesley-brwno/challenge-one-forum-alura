2087bd56e020e117a658602c23f46f2600c6d78f
wesley <wesleybruno910@gmail.com>
2023-09-15 15:46:55 -0300

    feat: adjust methos to work with TokenService
       * registrar now checks if the email is already being used, and returns a 409 Conflict HTTP reponse if it is
    
       * logar now uses the generateToken method from TokenService to get a JWT token, and returns it in the 200 OK response

--- src/main/java/com/br/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/com/br/alura/forum/controller/AutenticacaoController.java
@@ -1,21 +1,29 @@
 package com.br.alura.forum.controller;
 
+import com.br.alura.forum.DTO.autenticacao.ErrorConflict;
+import com.br.alura.forum.DTO.autenticacao.TokenDTO;
 import com.br.alura.forum.DTO.usuario.UsuarioDataInput;
 import com.br.alura.forum.DTO.usuario.UsuarioDataOutput;
 import com.br.alura.forum.DTO.usuario.UsuarioLogin;
+import com.br.alura.forum.infra.secutiry.TokenService;
 import com.br.alura.forum.modelo.Usuario;
 import com.br.alura.forum.repository.UsuarioRespository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/auth")
@@ -25,18 +33,30 @@ public class AutenticacaoController {
     private AuthenticationManager authenticationManager;
     @Autowired
     private UsuarioRespository usuarioRespository;
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping("/registrar")
-    public ResponseEntity<?> regisrar(@Valid @RequestBody UsuarioDataInput data) {
+    @Transactional
+    public ResponseEntity<?> regisrar(@Valid @RequestBody UsuarioDataInput data, UriComponentsBuilder uriComponentsBuilder) {
+        if (usuarioRespository.findByEmail(data.email()) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorConflict("Email já está em uso!"));
+        }
         String encode = new BCryptPasswordEncoder().encode(data.senha());
         Usuario usuario = new Usuario(data.nome(), data.email(), encode);
         usuarioRespository.save(usuario);
-        return ResponseEntity.ok().body(new UsuarioDataOutput(usuario.getId(), usuario.getNome(), usuario.getEmail(), data.senha().replaceAll(".", "*")));
+        URI uri = uriComponentsBuilder.path("/usuarios/{id}").buildAndExpand(usuario.getId()).toUri();
+        return ResponseEntity.created(uri).body(new UsuarioDataOutput(usuario));
     }
 
     @PostMapping("/entrar")
-    public ResponseEntity<Void> logar(@Valid @RequestBody UsuarioLogin data) {
+    public ResponseEntity<TokenDTO> logar(@Valid @RequestBody UsuarioLogin data) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(data.email(), data.senha());
         Authentication authenticate = authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
+        if (authenticate.isAuthenticated()) {
+            String tokenJWT = tokenService.generateTokenJWT((Usuario) authenticate.getPrincipal());
+            return ResponseEntity.ok().body(new TokenDTO(tokenJWT));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).header("WWW-Authenticate", "Bearer").build();
     }
 }
