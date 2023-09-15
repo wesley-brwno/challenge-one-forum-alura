ecf950f0ff69898f64e42f691195aaa71a050c9f
wesley <wesleybruno910@gmail.com>
2023-09-14 23:21:23 -0300

    feat: add findByEmail method to UsuarioRespository
    This method allows to find a user by its email address and returns a UserDetails object.
    
    The findByEmail() method can be used to authorize users and verify if they aren't already persisted to the database.

--- src/main/java/com/br/alura/forum/repository/UsuarioRespository.java
+++ src/main/java/com/br/alura/forum/repository/UsuarioRespository.java
@@ -4,9 +4,12 @@ import com.br.alura.forum.modelo.Usuario;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRespository extends JpaRepository<Usuario, Long> {
     Page<Usuario> findAllByIdNotNullOrderByNome(Pageable pageable);
+
+    UserDetails findByEmail(String email);
 }
