df14929764add98df510b3a371fa1ada9d9415aa
wesley <wesleybruno910@gmail.com>
2023-09-15 19:27:34 -0300

    feat: add getSubject method to TokenService
    This method verifies JWT Token and return the user who had sent it

--- src/main/java/com/br/alura/forum/infra/secutiry/TokenService.java
+++ src/main/java/com/br/alura/forum/infra/secutiry/TokenService.java
@@ -3,6 +3,7 @@ package com.br.alura.forum.infra.secutiry;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.br.alura.forum.modelo.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class TokenService {
 
     @Value("${api.securty.secret}")
     private String secret;
+
     public String generateTokenJWT(Usuario usuario) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
@@ -25,13 +27,26 @@ public class TokenService {
                     .withClaim("nome", usuario.getNome())
                     .withExpiresAt(expirationDate())
                     .sign(algorithm);
-        } catch (JWTCreationException exception){
+        } catch (JWTCreationException exception) {
             throw new RuntimeException("erro ao gerar token JWT", exception);
         }
     }
 
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Forum Alura")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
     private Instant expirationDate() {
-        return LocalDateTime.now().plusMinutes(3).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusMinutes(30).toInstant(ZoneOffset.of("-03:00"));
     }
 
 
